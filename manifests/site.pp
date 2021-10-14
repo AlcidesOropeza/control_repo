b45540ce343935d761f38046e2fc26eae29b84a3
Alcides Oropeza <92456239+AlcidesOropeza@users.noreply.github.com>
2021-10-13 21:17:05 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,7 @@
 node default {
   file {'/root/README':
     ensure  => file,
-    content => 'Ensure the Puppet server is running',
+    content => 'Ensure the Puppet server is running!!!>>>',
     owner   => 'root',
   } 
-  file {'/root/README':
-    owner => 'root',
-  }
 }
